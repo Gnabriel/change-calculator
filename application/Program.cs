d41992af1696af67331cfda5387aee3c2c004514
Gabriel Ellebrink <code.ngat4@simplelogin.co>
2022-06-30 10:24:34 +0200

    Delete windows form app

--- application/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChangeCalculatorApplication
-{
-    class ChangeCalculator {         
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("Hello World Application!");
-        }
-    }
-}
