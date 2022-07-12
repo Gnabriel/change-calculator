3593fc683a1dcf513bf4fedfe22309fa82fd49d8
Gabriel Ellebrink <code.ngat4@simplelogin.co>
2022-07-12 12:15:52 +0200

    Add logic from console and number inputs

--- winforms/Form1.Designer.cs
+++ winforms/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChangeCalculatorWinForms
 {
-  partial class Form1
+  partial class Form
   {
     /// <summary>
     ///  Required designer variable.
@@ -28,12 +28,104 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.btnCalculate = new System.Windows.Forms.Button();
+      this.lblCost = new System.Windows.Forms.Label();
+      this.lblPaid = new System.Windows.Forms.Label();
+      this.lblResult = new System.Windows.Forms.Label();
+      this.costInput = new System.Windows.Forms.NumericUpDown();
+      this.paidInput = new System.Windows.Forms.NumericUpDown();
+      ((System.ComponentModel.ISupportInitialize)(this.costInput)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.paidInput)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // btnCalculate
+      // 
+      this.btnCalculate.Location = new System.Drawing.Point(149, 289);
+      this.btnCalculate.Name = "btnCalculate";
+      this.btnCalculate.Size = new System.Drawing.Size(112, 34);
+      this.btnCalculate.TabIndex = 0;
+      this.btnCalculate.Text = "Calculate";
+      this.btnCalculate.UseVisualStyleBackColor = true;
+      this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+      // 
+      // lblCost
+      // 
+      this.lblCost.AutoSize = true;
+      this.lblCost.Location = new System.Drawing.Point(149, 98);
+      this.lblCost.Name = "lblCost";
+      this.lblCost.Size = new System.Drawing.Size(48, 25);
+      this.lblCost.TabIndex = 2;
+      this.lblCost.Text = "Cost";
+      this.lblCost.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // lblPaid
+      // 
+      this.lblPaid.AutoSize = true;
+      this.lblPaid.Location = new System.Drawing.Point(149, 187);
+      this.lblPaid.Name = "lblPaid";
+      this.lblPaid.Size = new System.Drawing.Size(117, 25);
+      this.lblPaid.TabIndex = 3;
+      this.lblPaid.Text = "Amount paid";
+      // 
+      // lblResult
+      // 
+      this.lblResult.AutoSize = true;
+      this.lblResult.Location = new System.Drawing.Point(451, 98);
+      this.lblResult.Name = "lblResult";
+      this.lblResult.Size = new System.Drawing.Size(0, 25);
+      this.lblResult.TabIndex = 5;
+      // 
+      // costInput
+      // 
+      this.costInput.Location = new System.Drawing.Point(149, 126);
+      this.costInput.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+      this.costInput.Name = "costInput";
+      this.costInput.Size = new System.Drawing.Size(180, 31);
+      this.costInput.TabIndex = 6;
+      // 
+      // paidInput
+      // 
+      this.paidInput.Location = new System.Drawing.Point(149, 215);
+      this.paidInput.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+      this.paidInput.Name = "paidInput";
+      this.paidInput.Size = new System.Drawing.Size(180, 31);
+      this.paidInput.TabIndex = 7;
+      // 
+      // Form
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.paidInput);
+      this.Controls.Add(this.costInput);
+      this.Controls.Add(this.lblResult);
+      this.Controls.Add(this.lblPaid);
+      this.Controls.Add(this.lblCost);
+      this.Controls.Add(this.btnCalculate);
+      this.Name = "Form";
       this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.costInput)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.paidInput)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private Button btnCalculate;
+    private Label lblCost;
+    private Label lblPaid;
+    private Label lblResult;
+    private NumericUpDown costInput;
+    private NumericUpDown paidInput;
   }
 }
--- winforms/Form1.cs
+++ winforms/Form1.cs
@@ -1,10 +1,160 @@
 namespace ChangeCalculatorWinForms
 {
-  public partial class Form1 : Form
+  // Class that is used to track number of bills and coins.
+  // Possible bills are 500, 200, 100, 50, and 20 kr.
+  // Possible coins are 10, 5, and 1 kr.
+  class CashBook
   {
-    public Form1()
+    // Bills.
+    public int num500s = 0;
+    public int num200s = 0;
+    public int num100s = 0;
+    public int num50s = 0;
+    public int num20s = 0;
+    // Coins.
+    public int num10s = 0;
+    public int num5s = 0;
+    public int num1s = 0;
+
+    // Creates a formatted string with the number of bills and coins.
+    public String toString()
+    {
+      string result = "";
+
+      if (num500s > 0)
+      {
+        result += $"Number of 500 kr bills: {num500s} \n";
+      }
+      if (num200s > 0)
+      {
+        result += $"Number of 200 kr bills: {num200s} \n";
+      }
+      if (num100s > 0)
+      {
+        result += $"Number of 100 kr bills: {num100s} \n";
+      }
+      if (num50s > 0)
+      {
+        result += $"Number of 50 kr bills:  {num50s} \n";
+      }
+      if (num20s > 0)
+      {
+        result += $"Number of 20 kr bills:  {num20s} \n";
+      }
+      if (num10s > 0)
+      {
+        result += $"Number of 10 kr coins:  {num10s} \n";
+      }
+      if (num5s > 0)
+      {
+        result += $"Number of 5 kr coins:   {num5s} \n";
+      }
+      if (num1s > 0)
+      {
+        result += $"Number of 1 kr coins:   {num1s} \n";
+      }
+
+      if (result == "")
+      {
+        return "No change.";
+      }
+
+      return result;
+    }
+  }
+
+  // Calculator class that contains the logic to calculate the change.
+  class Calculator
+  {
+    // Gets the change from the cost and amount paid as input.
+    // Returns NaN if the amount paid is less than the cost.
+    public static double getChange(double cost, double paidAmount)
+    {
+      if (paidAmount < cost)
+      {
+        return double.NaN;
+      }
+      return paidAmount - cost;
+    }
+
+    // Converts an amount to a number of different bills and coins.
+    // Returns a CashBook object.
+    public static CashBook amountToCash(double amount)
+    {
+      CashBook cashBook = new();
+
+      // Possible bill or coin values, sorted high to low.
+      int[] cashValues = new int[] { 500, 200, 100, 50, 20, 10, 5, 1 };
+
+      foreach (int cashValue in cashValues)
+      {
+        while (amount >= cashValue)
+        {
+          // Add the bill or coin to the cashBook.
+          switch (cashValue)
+          {
+            case 500:
+              cashBook.num500s++;
+              break;
+            case 200:
+              cashBook.num200s++;
+              break;
+            case 100:
+              cashBook.num100s++;
+              break;
+            case 50:
+              cashBook.num50s++;
+              break;
+            case 20:
+              cashBook.num20s++;
+              break;
+            case 10:
+              cashBook.num10s++;
+              break;
+            case 5:
+              cashBook.num5s++;
+              break;
+            case 1:
+              cashBook.num1s++;
+              break;
+            default:
+              break;
+          }
+          // Deduct the value of the bill or coin that we just added.
+          amount -= cashValue;
+        }
+      }
+
+      return cashBook;
+    }
+  }
+
+  public partial class Form : System.Windows.Forms.Form
+  {
+    public Form()
     {
       InitializeComponent();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void btnCalculate_Click(object sender, EventArgs e)
+    {
+      double changeAmount = Calculator.getChange(cost: Decimal.ToDouble(costInput.Value), paidAmount: Decimal.ToDouble(paidInput.Value));
+
+      if (double.IsNaN(changeAmount))
+      {
+        lblResult.Text = "Not enough paid.";
+        return;
+      }
+
+      // Get a CashBook object containing number of bills and coins.
+      CashBook cashBook = Calculator.amountToCash(amount: changeAmount);
+
+      lblResult.Text = cashBook.toString();
     }
   }
 }
